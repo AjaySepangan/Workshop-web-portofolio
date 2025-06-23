082687acac4a4fd6777f98cd5d7923f266193b03
Ajaysepangan <azaysepong@gmail.com>
2025-06-23 14:21:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ document.getElementById("contactForm").addEventListener("submit", function (e) {
     const name = document.getElementById("name").value.trim();
     const email = document.getElementById("email").value.trim();
     const message = document.getElementById("message").value.trim();
+    const msgBox = document.getElementById("formMessage");
 
     if (name && email && message) {
-        document.getElementById("formMessage").textContent = `Terima kasih, ${name}. Pesanmu sudah diterima!`;
+        msgBox.style.color = "green";
+        msgBox.textContent = `Terima kasih, ${name}. Pesanmu telah dikirim!`;
         this.reset();
     } else {
-        document.getElementById("formMessage").textContent = "Silakan isi semua kolom.";
+        msgBox.style.color = "red";
+        msgBox.textContent = "Harap isi semua kolom dengan benar.";
     }
 });
